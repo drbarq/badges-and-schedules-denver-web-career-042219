f926a7164e2937ed1a0c61a0f3800758267ccf0c
Joe Tustin <j.tustin@gmail.com>
2019-03-13 17:07:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -29,7 +29,7 @@ printer(["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
 =end
 
 def printer(array)
-  array.each {|name| puts batch_badge_creator(array)}
+  array.each {|name| puts batch_badge_creator(name)}
   array.each {|name| puts assign_rooms(array)}
   ##puts batch_badge_creator(array)
 
