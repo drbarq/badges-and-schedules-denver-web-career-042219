b65ec1e4ef59312fe4cc519fe03aa87705390df9
Joe Tustin <j.tustin@gmail.com>
2019-03-13 17:03:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,7 +25,7 @@ def printer(array)
   puts badge_name_room
 end
 
-printer(["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
+batch_badge_creator(["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
 =end
 
 def printer(array)
